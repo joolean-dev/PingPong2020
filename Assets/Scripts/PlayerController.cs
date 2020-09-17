fabb692e4aa7d15fe7cae814b61e1a9b129f0bdc
joolean-dev <me@joolean.dev>
2020-09-17 02:53:19 -0300

    Files cleanup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float pabloNoFlota = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
