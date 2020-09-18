640f14d8fc8681c13a0617f9dc9879794aad6d3c
joolean-dev <me@joolean.dev>
2020-09-18 09:52:03 -0300

    Updated project upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject validCourt;
+    private Touch touch;
+
+    float maxAxisX, maxAxisY;
+    public int resWidth, resHeight;
+
+    Vector3 camPos;
+    void Start()
+    {
+        resWidth = Screen.currentResolution.width;
+        resHeight = Screen.currentResolution.height;
+        maxAxisX = 12.0f;
+        maxAxisY = 7.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            //camPos = Camera.main.WorldToScreenPoint(touch.position);
+            //transform.position = camPos;
+
+          transform.position = new Vector3(
+                touch.position.x * maxAxisX / 1920f - (maxAxisX / 2),
+                transform.position.y,
+                touch.position.y * maxAxisY / 1080f - ((maxAxisY / 2 + transform.localScale.z))   );
+
+        }
+    } 
+}
---
+++ Assets/Scripts/ReduceTableByTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReduceTableByTime : MonoBehaviour
+{
+    Vector3 minScale;
+    public Vector3 maxScale;
+    public bool repeatable;
+    public float speed = 3f;
+    public float duration = 3f;
+
+    // Start is called before the first frame update
+    IEnumerator Start ()
+    {
+        minScale = transform.localScale;
+        while (repeatable)
+        {
+            //up scale
+            yield return RepeatLerp(minScale, maxScale, duration);
+            //down scale
+            yield return RepeatLerp(maxScale, minScale, duration);
+        }
+    }
+
+    public IEnumerator RepeatLerp (Vector3 a, Vector3 b, float time)
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        transform.localScale = Vector3.Lerp(a, b, i);
+        yield return null;
+    }
+    
+}
