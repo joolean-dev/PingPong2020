2b76448a239b05bb5a84082c4ad6badae458d652
panicoli0 <61359259+panicoli0@users.noreply.github.com>
2020-09-18 09:36:47 -0300

    toma puto
    putooo

---
+++ Assets/Scripts/ReduceTableByTime.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReduceTableByTime : MonoBehaviour
+{
+    Vector3 minScale;
+    public Vector3 maxScale;
+    public bool repeatable;
+    public float speed = 2f;
+    public float duration = 5f;
+
+    // Start is called before the first frame update
+    IEnumerator Start ()
+    {
+        minScale = transform.localScale;
+        while (repeatable)
+        {
+            //up scale
+            yield return RepeatLerp(minScale, maxScale, duration);
+            //down scale
+            yield return RepeatLerp(maxScale, minScale, duration);
+        }
+    }
+
+    public IEnumerator RepeatLerp (Vector3 a, Vector3 b, float time)
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            transform.localScale = Vector3.Lerp(a, b, i);
+            yield return null;
+        }
+    }
+    
+}
