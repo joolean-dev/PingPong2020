8daee60cb12ebcfb4c0f84c566f4024e37cddcd4
joolean-dev <me@joolean.dev>
2020-09-18 07:39:07 -0300

    Unity 2019.4.10f1 Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float pabloNoFlota = 0.0f;
+    public GameObject validCourt;
+    private Touch touch;
 
-    // Start is called before the first frame update
+    //float maxAxisX, maxAxisY;
+    public int resWidth, resHeight;
+
+    Vector3 camPos;
     void Start()
     {
-        
+        resWidth = Screen.currentResolution.width;
+        resHeight = Screen.currentResolution.height;
+        //maxAxisX = validCourt.transform.localScale.x;
+        //maxAxisY = validCourt.transform.localScale.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+
+        if (/*Input.touchCount > 0 ||*/ Input.GetMouseButtonDown(0))
+        {
+            //touch = Input.GetTouch(0);
+            camPos = Camera.main.WorldToScreenPoint(touch.position);
+            transform.position = camPos;
+
+        /*  Vector3 newDestination = new Vector3(
+                touch.position.x * maxAxisX / 1920f - (maxAxisX / 2),
+                this.transform.position.y,
+                touch.position.y * maxAxisY / 1080f - ((maxAxisY / 2 + transform.localScale.z))   );
+            
+            transform.position = newDestination;    */
+        }
+    } 
 }
