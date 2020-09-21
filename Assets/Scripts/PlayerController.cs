e7927de65728cc49a735fefe095d5610495abe90
joolean-dev <me@joolean.dev>
2020-09-21 11:15:08 -0300

    Added bouncing physics and collider between PlayerController & Ball

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,39 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject validCourt;
-    private Touch touch;
-
-    float maxAxisX, maxAxisY;
-    public int resWidth, resHeight;
-
-    Vector3 camPos;
-    void Start()
+    public float speed = 0.1f;
+    private Rigidbody rb;
+    //Sin uso de la función Start
+    /* void Start()
     {
-        /* resWidth = Screen.currentResolution.width;
-        resHeight = Screen.currentResolution.height; */
-        resWidth = 1920;
-        resHeight = 1080;
 
-        maxAxisX = 12.0f;
-        maxAxisY = 6.0f;
-    }
+    } */
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.touchCount > 0) {
+            //The screen has been touched so store the touch
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                // If the finger is on the screen, move the object smoothly to the touch 
+                Plane plane = new Plane(Vector3.up, transform.position);
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Input.touchCount > 0)
-        {
-            touch = Input.GetTouch(0);
-            //camPos = Camera.main.WorldToScreenPoint(touch.position);
-            //transform.position = camPos;
-
-          transform.position = new Vector3(
-                touch.position.x * maxAxisX / 1920f - (maxAxisX / 2),
-                transform.position.y,
-                touch.position.y * maxAxisY / 1080f - (maxAxisY / 2)    );
-
+                float dist;
+                if (plane.Raycast (ray, out dist)){
+                    transform.position = ray.GetPoint (dist);
+                }
+            }
         }
     } 
+
+    private void OnTriggerEnter(Collider other) {
+        rb = other.attachedRigidbody;
+
+        rb.AddForce(-0.02f, 0.03f, 0);
+    }
 }
