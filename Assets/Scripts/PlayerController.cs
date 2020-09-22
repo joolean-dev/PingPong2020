f0e106767138f44445e0a25eb364bf3112e55e50
joolean-dev <me@joolean.dev>
2020-09-22 08:53:17 -0300

    Finger touch angle calculation in progress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,15 +27,31 @@ public class PlayerController : MonoBehaviour
                 float dist;
                 if (plane.Raycast (ray, out dist)){
                     transform.position = ray.GetPoint (dist);
+                    //Debug.Log("In the inside");
+
+
+                    Vector3 dir = new Vector3(touch.position.x, transform.position.y, touch.position.y) - transform.position;
+                    float angle = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.AngleAxis (angle, Vector3.up);
+                    Debug.Log("Angulo: " + angle);
                 }
             }
         }
     } 
 
     private void OnTriggerEnter(Collider other) {
-        Debug.Log("Otup el que lee");
+
         rb = other.attachedRigidbody;
 
-        rb.AddForce(-0.3f, 0.3f, 0);
+        //rb.AddForce(-0.3f, 0.3f, 0);
+        AddForceAtAngle(-0.3f, 90f, rb);
+    }
+
+    public void AddForceAtAngle(float force, float angle, Rigidbody rb)
+    {
+    float xcomponent = Mathf.Cos(angle * Mathf.PI / 180) * force;
+    float ycomponent = Mathf.Sin(angle * Mathf.PI / 180) * force;
+    
+    rb.AddForce(ycomponent, 0, xcomponent);
     }
 }
