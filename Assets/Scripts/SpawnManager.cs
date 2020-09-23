9ea1adec3c83c50bfb11e3e9288679fe2124d990
panicoli0 <pablo.nicoli@hotmail.com>
2020-09-23 07:44:11 -0300

    improve SM

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,26 +8,31 @@ public class SpawnManager : MonoBehaviour
     private float spawnDelay = 2;
     private float spawnInterval = 1.5f;
 
-    private PlayerController playerControllerScript;
+    private float spawnLimitXLeft = -3;
+    private float spawnLimitXRight = 3;
+    private float spawnPosY = 2; //Altura de caida powerup
+    private float spawnLimitZLeft = 1.5f;
+    private float spawnLimitZRight = -1.5f;
+
+    //private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
-        playerControllerScript = GameObject.Find("Plane").GetComponent<PlayerController>();
+        //playerControllerScript = GameObject.Find("Plane").GetComponent<PlayerController>();
     }
 
     // Spawn power-ups
     void SpawnObjects()
     {
         // Set random spawn location and random object index
-        Vector3 spawnLocation = new Vector3(-2, Random.Range(5, 15), 0);
+        Vector3 spawnLocation = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, Random.Range(spawnLimitZLeft, spawnLimitZRight));
         int index = Random.Range(0, objectPrefabs.Length);
 
         // If game is still active, spawn new object
-        if (!playerControllerScript.gameOver)
-        {
-            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
-        }
+        
+        Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        
     }
 }
